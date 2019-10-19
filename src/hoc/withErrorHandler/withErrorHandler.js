b11a5129057b7c4eab13c4ac7e3ea8db201c4546
Georgy Gerbst <gerbst.g@gmail.com>
2019-10-19 10:53:05 +0300

    br commit

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -21,6 +21,7 @@ const withErrorHandler = ( Wrapped, axios ) => {
         };
 
         componentWillUnmount() {
+            console.log(12312312213);
             axios.interceptors.request.eject(this.requestInterceptor);
             axios.interceptors.response.eject(this.responseInterceptor);
         }
